bf013a9e1bdf3a640aca52c72dfeef01a43c6931
nicol <nicol@ORDI-NICO>
2025-06-18 16:13:51 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int nombre;
+
+Console.WriteLine("Saisissez un nombre :");
+nombre = Convert.ToInt32(Console.ReadLine());
+
+if (nombre > 10)
+{
+    Console.WriteLine("supérieur");
+}
+else
+{
+    Console.WriteLine("inférieur");
+}
+;
