0317b3c82251ef777d7b38b0a906359daec27b20
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-06-19 18:06:23 +0200

    feat: add exercice 22

--- Program.cs
+++ Program.cs
@@ -169,3 +169,12 @@ for (int i = 0; i < mot.Length ; i++)
 {
     Console.WriteLine(i);
 };*/
+
+//22. Afficher les nombres pairs entre 1 et 20.
+
+/*for(int i = 1; i <= 20; i++)
+{
+    if (i %2 == 0) {
+        Console.WriteLine(i);
+    }
+};*/
