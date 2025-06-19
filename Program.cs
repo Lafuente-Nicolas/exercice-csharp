a2464f0a4657e83a661ab99b392e57f831b180dc
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-06-19 14:42:05 +0200

    feat: add exercice

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int nombre;
+﻿// Écrire un programme qui vérifie si un nombre donné est supérieur à 10.
+/*int nombre;
 
 Console.WriteLine("Saisissez un nombre :");
 nombre = Convert.ToInt32(Console.ReadLine());
@@ -11,4 +12,67 @@ else
 {
     Console.WriteLine("inférieur");
 }
-;
+;*/
+// Vérifier si un nombre donné est positif, négatif ou nul.
+
+
+/*int nombre;
+Console.WriteLine("saisissez un nombre :");
+nombre = Convert.ToInt32(Console.ReadLine());
+if (0 < nombre * -1)
+{
+    Console.WriteLine("négatif");
+}
+else if (0 < nombre)
+{
+    Console.WriteLine("positif");
+}
+else
+{
+    Console.WriteLine("Nul");
+};*/
+
+// Créer un programme qui affiche "Vous êtes mineur" si l’âge est inférieur à 18.
+
+/*int nombre;
+Console.WriteLine("Saisissez votre âge :");
+nombre = Convert.ToInt32(Console.ReadLine());
+if (nombre < 18) 
+{
+    Console.WriteLine("vous êtes mineur");
+}
+else
+{
+    Console.WriteLine("Vous êtes majeur");
+};*/
+
+// Écrire un programme qui vérifie si une chaîne de caractères est vide ou non.
+
+/*String mots;
+Console.WriteLine("saisissez une chaine de caractères :");
+mots = Console.ReadLine();
+
+if (mots == "")
+{ 
+    Console.WriteLine("La chaîne de caractères est vide.");
+}
+else
+{
+        Console.WriteLine("La chaîne de caractères n'est pas vide.");
+};*/
+
+//  Vérifier si un nombre est pair ou impair.
+/*
+int number;
+Console.WriteLine("Indiquez votre age");
+number = Convert.ToInt32(Console.ReadLine());
+
+if ( 0 >number*-1)
+{
+    Console.WriteLine("C'est un nombre impair");
+}
+else
+{
+    Console.WriteLine("C'est un nombre pair");
+}*/
+
